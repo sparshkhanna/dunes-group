f936c8c83ab193138e32de99abca8aa67b9f069e
sparshkhanna <47217816+sparshkhanna@users.noreply.github.com>
2025-06-29 22:30:33 +0530

    Add custom color palette to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gold: {
+                    dark: '#C78D17', // Dark goldenrod
+                    metallic: '#D7AC28', // Gold (metallic)
+                },
+                purple: {
+                    pale: '#EFD9F7', // Pale purple
+                    grape: '#8238B3', // Grape
+                    indigo: '#3D0066', // Indigo
+                },
+            },
+        },
     },
     plugins: [],
 } 
